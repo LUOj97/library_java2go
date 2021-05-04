a6ff4c7b22f2f1d837f95d78f93592c30ce9f632
Luoj <luoj@pc-132-149.customer.ask4.lan>
2021-05-04 10:32:03 +0100

    first commit

---
+++ main/main.go
@@ -0,0 +1,8 @@
+package main
+
+import "library_java2go/test"
+
+func main() {
+	word := "hello world"
+	test.Print(word)
+}
---
+++ test/print.go
@@ -0,0 +1,7 @@
+package test
+
+import "fmt"
+
+func Print(args string) {
+	fmt.Print(args)
+}
